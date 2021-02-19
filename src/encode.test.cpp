a1308eb5b5ae3ffda692308593071b62eaad4aa1
Jordan Williams <jordan@jwillikers.com>
2021-02-19 10:47:53 -0600

    Format

--- src/encode.test.cpp
+++ src/encode.test.cpp
@@ -1,5 +1,5 @@
-#include <cyrillic-encoder/encode.hpp>
 #include <boost/ut.hpp>
+#include <cyrillic-encoder/encode.hpp>
 #include <string_view>
 
 namespace test {
@@ -7,20 +7,20 @@ namespace test {
 using namespace boost::ut;
 
 suite encode_char = [] {
-
   "should_return_an_empty_string_given_null"_test = [] {
     static constexpr char input{'\0'};
     expect(std::empty(cyr_enc::encode_char(input)));
   };
 
-  "should_return_a_different_cyrillic_string_for_each_case_of_a_character"_test = [] {
-    static constexpr char lowercase{'a'};
-    static constexpr char uppercase{'A'};
-    static constexpr std::string_view encoded_lowercase{"г"};
-    static constexpr std::string_view encoded_uppercase{"Њ"};
-    expect(encoded_lowercase == cyr_enc::encode_char(lowercase));
-    expect(encoded_uppercase == cyr_enc::encode_char(uppercase));
-  };
+  "should_return_a_different_cyrillic_string_for_each_case_of_a_character"_test =
+      [] {
+        static constexpr char lowercase{'a'};
+        static constexpr char uppercase{'A'};
+        static constexpr std::string_view encoded_lowercase{"г"};
+        static constexpr std::string_view encoded_uppercase{"Њ"};
+        expect(encoded_lowercase == cyr_enc::encode_char(lowercase));
+        expect(encoded_uppercase == cyr_enc::encode_char(uppercase));
+      };
 
   "should_return_the_same_cyrillic_string_for_a_character"_test = [] {
     static constexpr char input{'a'};
@@ -28,20 +28,22 @@ suite encode_char = [] {
     expect(encoded == cyr_enc::encode_char(input));
   };
 
-//  "should_return_unique_values_for_each_alphanumeric_character"_test = [] {
-//    static constexpr std::array<char, 62> alphanumeric_characters{{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'}};
-//    expect(boost::size(cyr_enc::sample_conversion_table | boost::adaptors::map_keys | boost::adaptors::uniqued) == cyr_enc::num_alphanumeric);
-//  };
+  //  "should_return_unique_values_for_each_alphanumeric_character"_test = [] {
+  //    static constexpr std::array<char, 62> alphanumeric_characters{{'0', '1',
+  //    '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F',
+  //    'G', 'H', 'I', 'J', 'K', 'L', 'M'}};
+  //    expect(boost::size(cyr_enc::sample_conversion_table |
+  //    boost::adaptors::map_keys | boost::adaptors::uniqued) ==
+  //    cyr_enc::num_alphanumeric);
+  //  };
 
   "should_return_an_empty_string_for_a_non_alphanumeric_character"_test = [] {
-  static constexpr char input{'\t'};
-  expect(std::empty(cyr_enc::encode_char(input)));
+    static constexpr char input{'\t'};
+    expect(std::empty(cyr_enc::encode_char(input)));
   };
-
 };
 
 suite encode_string = [] {
-
   "should_return_an_empty_string_for_a_non_alphanumeric_characters"_test = [] {
     static constexpr std::string_view input{"!?!"};
     expect(std::empty(cyr_enc::encode_string(input)));
@@ -54,9 +56,8 @@ suite encode_string = [] {
   };
 
   // todo More tests.
-
 };
 
-}
+} // namespace test
 
 int main() {}
