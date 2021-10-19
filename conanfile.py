c77dfe7b99e83cdbcae60a0c191e37c0a0896f3e
Jordan Williams <jordan@jwillikers.com>
2021-10-19 18:41:41 -0500

    Cleanup Conan file

--- conanfile.py
+++ conanfile.py
@@ -1,4 +1,5 @@
-from conans import ConanFile, CMake
+from conans import ConanFile
+from conan.tools.cmake import CMake
 import os
 
 
@@ -8,29 +9,22 @@ class CyrillicEncoder(ConanFile):
     url = "https://github.com/jwillikers/cyrillic-encoder"
     description = " A demo Qt application for encoding alphanumeric characters as arbitrary Cyrillic symbols"
     settings = "arch", "build_type", "compiler", "os"
-    generators = "CMakeDeps", "CMakeToolchain"
-    requires = "boost/1.77.0", "boost-ext-ut/1.1.8", "icu/69.1", "ms-gsl/3.1.0", "qt/5.15.2"
+    generators = "CMakeDeps", "CMakeToolchain", "VirtualRunEnv"
+    requires = "boost/1.77.0", "boost-ext-ut/1.1.8", "icu/69.1", "ms-gsl/3.1.0", "qt/5.15.2@qtwayland/testing"
     default_options = {
         "boost:i18n_backend": "icu",
         "boost:without_stacktrace": True,
         "qt:qtwayland": True,
     }
     scm = {"type": "git", "url": "auto", "revision": "auto", }
-    _cmake = None
-
-    def configure_cmake(self):
-        if self._cmake is not None:
-            return self._cmake
-        self._cmake = CMake(self)
-        self._cmake.configure()
-        return self._cmake
 
     def build(self):
-        cmake = self.configure_cmake()
+        cmake = CMake(self)
+        cmake.configure()
         cmake.build()
 
     def package(self):
-        cmake = self.configure_cmake()
+        cmake = CMake(self)
         cmake.install()
 
     def package_info(self):
